874f5dab322532109eb9a25c1be0d0a95ea46044
unknown <rajputnikita5666@gmail.com>
2021-09-17 13:41:36 +0530

    Adding Welcome Message

---
+++ com/LineComparision/LineComparision.java
@@ -0,0 +1,12 @@
+package com.LineComparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		// Welcome message
+		
+		System.out.println("WELCOME TO LINE COMPARISION PROGRAM");
+
+	}
+
+}
---
+++ com/LineComparision/WelcomeLC.java
@@ -0,0 +1,12 @@
+package com.LineComparision;
+
+public class WelcomeLC {
+
+	public static void main(String[] args) {
+		// Welcome Message
+		
+		System.out.println("WELCOME TO LINE COMPARISION");
+
+	}
+
+}
